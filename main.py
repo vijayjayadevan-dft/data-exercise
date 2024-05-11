f4bb6f2a7532ff72a534d9ec54288734396bd455
Vijay Jayadevan <97686721+vijayjayadevan-dft@users.noreply.github.com>
2024-05-11 04:54:37 +0000

    new file:   config.cfg 	renamed:    input/daily_files/crm_campaign_20230101001500.json -> data/input/daily_files/crm_campaign_20230101001500.json 	renamed:    input/daily_files/crm_engagement_20230101001500.json -> data/input/daily_files/crm_engagement_20230101001500.json 	modified:   main.py 	new file:   myenv/bin/coverage 	new file:   myenv/bin/coverage-3.10 	new file:   myenv/bin/coverage3 	new file:   myenv/bin/fab 	new file:   myenv/bin/inv 	new file:   myenv/bin/invoke 	new file:   myenv/bin/jp.py 	new file:   myenv/bin/pep8 	modified:   myenv/bin/pip3 	modified:   myenv/bin/pip3.10 	new file:   myenv/bin/ppp 	modified:   requirements.txt

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
+import sys
+import os
+import boto3
+import configparser
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType,BooleanType,DoubleType,ArrayType
 from pyspark.sql.functions import explode, explode_outer, col, count, size
 
-def main():
+
+config = configparser.ConfigParser()
+config.read('config.cfg')
+
+def create_spark_session(profile):
+    """Create a Spark session to process the data
+
+    Arguments: None
+
+    Returns:
+        spark: a Spark session
+    """
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
+    if profile != 'local':
+      session = boto3.session.Session(profile_name = profile)
+      spark.sparkContext._jsc.hadoopConfiguration().set('fs.s3n.awsAccessKeyId', session.get_credentials().access_key)
+      spark.sparkContext._jsc.hadoopConfiguration().set('fs.s3n.awsSecretAccessKey', session.get_credentials().secret_key)
+       
+    return spark
+
+
+
+def main(config_name):
     # create a SparkSession
-    spark = SparkSession.builder.appName("ReadAllJSONFiles").getOrCreate()
+    #spark = SparkSession.builder.appName("ReadAllJSONFiles").getOrCreate()
+    # Replace Key with your AWS account accesskey (You can find this on IAM 
+    profile = config[config_name]["PROFILE"]
+    internal_data = config[config_name]["INTERNAL_DATA"]
+    input_data = config[config_name]["INPUT_DATA"]
+    output_data = config[config_name]["OUTPUT_DATA"]
+    spark = create_spark_session(profile)
 
     # Read the JSON file into a DataFrame
-    df = spark.read.option("multiline","true").json("input/daily_files/crm_campaign_20230101001500.json")
+    df = spark.read.option("multiline","true").json(input_data + "/crm_campaign_20230101001500.json")
 
     df_flat = flatten_json(df)
     campaign_report = df_flat.select(col('id').alias('campaign_id'),
@@ -19,7 +54,7 @@ def main():
     campaign_report.show()
 
     # Read the JSON file into a DataFrame
-    df = spark.read.option("multiline","true").json("input/daily_files/crm_engagement_20230101001500.json")
+    df = spark.read.option("multiline","true").json(input_data + "/crm_engagement_20230101001500.json")
     df.printSchema()
     df.show()
 
@@ -62,4 +97,4 @@ def flatten_json(df):
 #                             if type(field.dataType) == ArrayType or  type(field.dataType) == StructType])
    return df
 
-main()
+main('DEFAULT')
